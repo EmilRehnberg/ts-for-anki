f6d21080f63fee2b343deea6a2b7c73a09f2ec14
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-12-14 20:20:21 +0100

    add jest settings

---
+++ jest.js
@@ -0,0 +1,8 @@
+process.on("uncaughtException", function(err) {
+  let msg = "Uncaught exception: " + err.stack;
+  console.error(msg);
+});
+
+module.exports = async () => {
+  process.env.NODE_ENV = "test";
+};
